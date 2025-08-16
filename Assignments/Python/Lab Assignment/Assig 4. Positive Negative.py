822f59589c74a532fdbb13e66a9ed17f88d25170
Juhi Dhruv <juhidhruv2@gmail.com>
2025-08-16 09:47:28 +0530

    Add files via upload

---
+++ Assignments/Python/Lab Assignment/Assig 4. Positive Negative.py
@@ -0,0 +1,7 @@
+number=int(input("enter your number"))
+if number>0:
+    print(f"{number} is positive")
+if number==0:
+    print(f"{number} is zero")
+if number<0:
+    print(f"{number} is negative")
